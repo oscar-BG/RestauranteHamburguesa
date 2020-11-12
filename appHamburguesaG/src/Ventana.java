3ab5d1a03414861cb03b6319e6cc46cb2f8bba0a
Oscar Bautista <oscar04262000@gmail.com>
2020-11-11 23:54:37 -0600

    Agregamos un JPanel y JLabel, modificamos las propiedades de ambos

--- appHamburguesaG/src/Ventana.java
+++ appHamburguesaG/src/Ventana.java
@@ -1,15 +1,38 @@
 import javax.swing.JFrame;
-
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Dimension;
+import java.awt.Color;
 public class Ventana  extends  JFrame{
 	
+	private JPanel panel;
+	private JLabel lblTitulo;
+	
 	public Ventana(){
+		
 		setTitle("Hamburguesa");
 		setSize(500,500); //Tamaño de la ventana
-		//setLocation(500,200); //(x,y)Punto inicial de la ventana
-		//setBounds(500,200,500,500); //(x,y,w,h)
-		setLocationRelativeTo(null);
+		setLocationRelativeTo(null);//Muestra la ventana en el centro de la pantall
 		setDefaultCloseOperation(EXIT_ON_CLOSE);//Cerrar ventana
 		
+		componentes();
+	}
+	
+	private  void componentes(){
+		panel = new JPanel(); //Creacion de un pannel
+		panel.setLayout(null);//Desactivar los diseños por defecto
+		panel.setBackground(Color.YELLOW);
+		this.getContentPane().add(panel);//Agregamos el panel a la ventana
+		
+		//Agregamos etiqueta al panel
+		
+		lblTitulo = new JLabel();
+		//lblTitulo.setOpaque(true); //Conceder permiso para intar
+		lblTitulo.setText("Hamburguesa");//Establecemos el texto en la etiqueta
+		lblTitulo.setBounds(200,10,100,10);//(x,y,ancho,alto)
+		lblTitulo.setForeground(Color.RED); //Establecemos el color de la letra
+		//lblTitulo.setBackground(Color.WHITE); //Cambiamos el color de fotdo
+		panel.add(lblTitulo);//Añadimos la etiqueta al panel
 	}
 	
 	
